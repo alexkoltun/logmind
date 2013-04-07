14f133f7ecd037fd4be4a74819a12f1980921cf2
ohanetz <o.hanetz@gmail.com>
2013-04-07 18:29:38 +0300

    Added windows agent to repository.
    Signed-off-by: ohanetz <o.hanetz@gmail.com>

---
+++ agent/platform/windows/evLogReader/LogLocalStorage.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace evLogReader
+{
+    [XmlRoot("localStorage")]
+    public class LogLocalStorage
+    {
+        private string m_logName;
+
+        [XmlElement("lastLogGenerated")]
+        public DateTime LastLogGenerated { get; set; }
+
+        [XmlIgnore()]
+        public bool IsDirty { get; set; }
+
+        private static string GetFilePath(string logName)
+        {
+            var currentFolder = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            return Path.Combine(currentFolder, string.Format("storage_{0}.xml", logName));
+        }
+
+        public static LogLocalStorage Load(string logName)
+        {
+            string file = GetFilePath(logName);
+
+            LogLocalStorage retVal = null;
+
+            if (File.Exists(file))
+            {
+                try
+                {
+                    using (StreamReader reader = new StreamReader(file))
+                    {
+                        XmlSerializer ser = new XmlSerializer(typeof(LogLocalStorage));
+                        retVal = (LogLocalStorage)ser.Deserialize(reader);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    retVal = new LogLocalStorage();
+                }
+            }
+            else
+            {
+                retVal = new LogLocalStorage();
+            }
+            
+            retVal.m_logName = logName;
+            return retVal;
+        }
+
+        public void Save()
+        {
+            if (IsDirty == false)
+                return;
+
+            string file = GetFilePath(m_logName);
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(file))
+                {
+                    XmlSerializer ser = new XmlSerializer(typeof(LogLocalStorage));
+                    ser.Serialize(writer, this);
+                    IsDirty = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                LocalLogger.Write(ex);
+            }
+
+        }
+    }  
+}
