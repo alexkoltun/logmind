fce341254436150a5d3cdf4d25b2845984621c57
ohanetz <o.hanetz@gmail.com>
2013-05-17 00:14:34 +0300

    Updated installer:  - Setting up an elasticsearch cluster using argument/user input, and updating configuration accordingly.  - Added prerequisites section. Currently, making sure Java is installed.  - Added support for multi-colored messages.

--- install/linux/install.py
+++ install/linux/install.py
@@ -3,10 +3,20 @@
 import os
 import sys
 import shutil
-from subprocess import call, Popen
+from subprocess import call
+
 
 
 LOGMIND_PATH = "/usr/local/logmind"
+
+
+class ShellColors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
 
 
 #######################
@@ -21,6 +31,14 @@ def prompt(msg):
     return res
 
 
+def user_input(msg):
+    res = ""
+    while not len(res) > 0:
+        res = raw_input(msg)
+
+    return res
+
+
 def uninstall_previous():
     if os.path.exists(LOGMIND_PATH):
         shutil.rmtree(LOGMIND_PATH)
@@ -30,8 +48,26 @@ def uninstall_previous():
 
     if os.path.exists("/command"):
         shutil.rmtree("/command")
-                      
 
+
+def check_prereq():
+    try:
+        print "Checking for Java...",
+        java_ok = os.system("java -version") == 0
+        if java_ok:
+            print "[ " + ShellColors.OKGREEN + "OK" + ShellColors.ENDC + " ]"
+            
+        else:
+            print "[ " + ShellColors.FAIL + "FAIL" + ShellColors.ENDC + " ]"
+            print ShellColors.FAIL + "Please make sure Java is installed and JAVA_HOME is set, and try again" + ShellColors.ENDC
+            return False
+
+        return True
+
+    except Exception, e:
+        print "ERROR: ", e
+        return False
+    
 
 def copy_files():
     try:
@@ -148,25 +184,48 @@ def prep_links(curdir):
     except Exception, e:
         print "ERROR: ", e
         return False
-    return True
 
 
-def finalize_inst():
+def update_file(file_path, old_string, new_string):
+    f = open(file_path, "rb")
+    f_str = f.read()
+    f.close()
+
+    f_str = f_str.replace(old_string, new_string)
+    f = open(file_path, "wb")
+    f.write(f_str)
+    f.close()
+
+
+def post_install():
 
     try:
-
-        if "Ubuntu" in os.uname()[3]:
-            print "Starting services on Ubuntu Linux OS..."
-
-            pid = Popen(["svscanboot"]).pid
-            print "svscanboot started with PID", pid
-
+        arg_index = -1
+        if "--cluster-name" in sys.argv:
+            arg_index = sys.argv.index("--cluster-name")
+        if arg_index > -1 and len(sys.argv) > arg_index + 1:
+            cluster_name = sys.argv[arg_index + 1]
+        else:
+            cluster_name = user_input("Please enter unique Logmind installation name (ElasticSearch Cluster name): ")
+            
+        print "Using cluster name: ", cluster_name
         
+        es_conf_file = "/".join((LOGMIND_PATH, "elasticsearch", "config", "elasticsearch.yml"))
+        update_file(es_conf_file, "[LOGMIND_ES_CLUSTER_NAME]", cluster_name)
+        
+        indexer_conf_file = "/".join((LOGMIND_PATH, "sixthsense", "config", "indexer.conf", "logmind-indexer.conf"))
+        update_file(indexer_conf_file, "[LOGMIND_ES_CLUSTER_NAME]", cluster_name)
+        
+        
+
         return True
 
     except Exception, e:
         print "ERROR: ", e
         return False
+
+        
+
 
 
 
@@ -177,25 +236,29 @@ def finalize_inst():
 
 def __main__() :
 
-    print "Installing Logmind to", LOGMIND_PATH
+    print "Checking prerequisites..."
+    if check_prereq():
 
-    print "Copying files..."
-    if copy_files():
+        print "Installing Logmind to", LOGMIND_PATH
 
-        print "Setting permissions..."
-        if set_permissions():
+        print "Copying files..."
+        if copy_files():
 
-            if set_attrs():
+            print "Setting permissions..."
+            if set_permissions():
 
-                print "Installing services..."
-                curdir = os.path.abspath(os.curdir)
+                if set_attrs():
 
-                if inst_daemon(curdir):
+                    print "Installing services..."
+                    curdir = os.path.abspath(os.curdir)
 
-                    if prep_links(curdir):
+                    if inst_daemon(curdir):
 
-                        if finalize_inst():
-                            print "Logmind installed successfully!"
+                        if prep_links(curdir):
+ 
+                            print "Running post-installation operations..."
+                            if post_install():
+                                print ShellColors.OKGREEN + "Logmind installed successfully!" + ShellColors.ENDC
 
 
 
