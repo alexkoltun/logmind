74cee70cfff5ef5647754190fd53a912736fe561
alexkoltun <jergold1!>
2013-08-08 19:40:19 +0300

    fixes

--- system/archiver.rb
+++ system/archiver.rb
@@ -5,14 +5,19 @@ require 'logger'
 require 'time'
 require 'fileutils'
 require 'net/http'
+require 'yaml'
 require 'pp'
 
 module GlobalConfig
-  LOCAL_INDICES_DIR = ENV['LOGMIND_ES_INDICES_DIR'] ||  '/var/lib/elasticsearch/AlexKoltunDevLinux/nodes/0/indices'
+  LOCAL_INDICES_DIR = ENV['LOGMIND_ES_INDICES_DIR'] ||  '/usr/local/logmind/elasticsearch/data'
 end
 
 class IndexMatcher
   def initialize(pattern, older_than)
+
+    es_config = YAML.load_file(ENV['LOGMIND_ES_CONFIG_YAML'] || '/usr/local/logmind/elasticsearch/config/elasticsearch.yml')
+    @es_cluster_name = es_config['cluster.name'] || 'elasticsearch'
+
     @older_than = older_than
     @pattern = pattern
 
@@ -24,6 +29,10 @@ class IndexMatcher
     end
 
     @time_format && @time_format = @time_format.gsub('YYYY', '%Y').gsub('YY', '%Y').gsub('MM', '%m').gsub('dd', '%d')
+  end
+
+  def get_es_indices_dir
+    GlobalConfig::LOCAL_INDICES_DIR + '/' + @es_cluster_name + '/nodes/0/indices'
   end
 
   def match(index)
@@ -43,24 +52,39 @@ class Archiver
 
   def run
 
+    action_map = nil
 
-    action_map = [
-        {
-            :pattern => 'logstash-wd-%{+YYYY}',
-            :size_min => 1024*1024,
-            :size_max => nil,
-            :older_than => 60*60*24*7, # seven days
-            :action => 'RELOCATE',
-            :params => { :target => '/tmp'}
-        }
-    ]
+#    action_map = [
+#        {
+#            :pattern => 'logstash-wd-%{+YYYY}',
+#            :size_min => 1024*1024,
+#            :size_max => nil,
+#            :older_than => 60*60*24*7, # seven days
+#            :action => 'RELOCATE',
+#            :params => { :target => '/tmp'}
+#        }
+#    ]
 
     # load action map from elasticsearch
+    actions_raw_respone = @http_client.get('/logmind-management/archive-action/_search?size=1000')
+    actions_response = JSON.parse(actions_raw_respone.body)
+
+    if(actions_response['ok'])
+      action_map = actions_response['hits']['hits']
+    else
+      @log.error { 'Unable ro get actions from elasticsearch, response: ' + actions_raw_respone.body }
+      return -1
+    end
 
 
     # get stats for all indices
     stats_raw_respone = @http_client.get('/*/_stats/')
     stats_response = JSON.parse(stats_raw_respone.body)
+
+    if !stats_response['ok']
+      @log.error { 'Unable ro get indices stats, response: ' + stats_raw_respone.body }
+      return -2
+    end
 
     # walk over indices stats and see if archiving operation is needed for them
     action_map.each do |item|
@@ -96,14 +120,21 @@ class Archiver
         end
       when 'RELOCATE'
         @log.info { 'Relocating index: ' + index + ', target: ' + item[:params][:target] }
+# 0. check if we already relocated the index to the desired target
+        index_dir = get_es_indices_dir + '/' + index
+        index_target_dir = item[:params][:target] + '/' + index
+
+        if Pathname.new(index_dir) == Pathname.new(index_target_dir) || (File.symlink?(index_dir) && Pathname.new(File.readlink(index_dir)) == Pathname.new(index_target_dir))
+          @logger.info { 'Ignoring RELOCATE command for index: ' + index + ', reason: it seems that the index was already relocated.' }
+          return 0
+        end
+
 # 1. close the index
         close_raw_response = @http_client.post('/' + index + '/_close', '')
         close_response = JSON.parse(close_raw_response.body)
 
         if close_response['ok']
 # 2. copy the existing index data to the new location
-          index_dir = GlobalConfig::LOCAL_INDICES_DIR + '/' + index
-          index_target_dir = item[:params][:target] + '/' + index
           FileUtils.copy_entry index_dir, index_target_dir
           ## recovery: reopen the index
 # 3. unlink the old index data 'link', optionally remove the files as well
