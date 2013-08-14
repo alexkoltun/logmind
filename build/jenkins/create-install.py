b7108829ef223754870a2e32a7f65419260a765d
ohanetz <o.hanetz@gmail.com>
2013-08-14 10:48:30 +0300

    Updated create-install.py script to pass all the command-line arguments to the inner install script, in order to allow support for multiple installation types in rpm.
    Signed-off-by: ohanetz <o.hanetz@gmail.com>

--- build/jenkins/create-install.py
+++ build/jenkins/create-install.py
@@ -3,6 +3,6 @@ import sys
 
 dir_name = sys.argv[1]
 f = open("/logmind/build/build/jenkins/install.sh", "wb")
-f.writelines(["#!/bin/sh", "\n", "/".join(("", dir_name, "install"))])
+f.writelines(["#!/bin/sh", "\n", "/".join(("", dir_name, "install")) + "\"$@\""])
 f.close()
 
