aee1630594cbd4eb4179853bdc11feae44494461
ohanetz <o.hanetz@gmail.com>
2013-08-14 11:56:33 +0300

    Updated create-install.py script to pass all the command-line arguments to the inner install script, in order to allow support for multiple installation types in rpm.
    Signed-off-by: ohanetz <o.hanetz@gmail.com>

--- build/jenkins/create-install.py
+++ build/jenkins/create-install.py
@@ -2,7 +2,18 @@
 import sys
 
 dir_name = sys.argv[1]
-f = open("/logmind/build/build/jenkins/install.sh", "wb")
-f.writelines(["#!/bin/sh", "\n", "/".join(("", dir_name, "install")) + "\"$@\""])
+install_type = sys.argv[2] # server, server-upgrade, client, client-upgrade
+f = open("/logmind/build/build/jenkins/install" + install_type + ".sh", "wb")
+f.writelines(["#!/bin/sh", "\n", "/".join(("", dir_name, "install")) + " "])
+
+if install_type == "server":
+	f.write("-type server")
+elif install_type == "server-upgrade":
+	f.write("-type server -mode upgrade -backup")
+elif install_type == "client":
+	f.write("-type client")
+elif install_type == "server":
+	f.write("-type client -mode upgrade -backup")
+
 f.close()
 
